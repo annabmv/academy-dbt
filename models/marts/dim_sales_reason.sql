33e2f8d8bb376d92c0f8168b9153c04466cf3986
Anna Beatriz Vercesi <annabm.vercesi@gmail.com>
2024-02-27 23:09:11 +0000

    feat: add dimension table for sales reason

---
+++ models/marts/dim_sales_reason.sql
@@ -0,0 +1,10 @@
+with
+    dim_sales_reason as (
+        select 
+            {{ dbt_utils.generate_surrogate_key(['sales_order_id', 'sales_reason_id']) }} as sk_sales_reason
+            , *
+        from{{ ref('int_sales_reason')}}
+    )
+
+select *
+from dim_sales_reason
